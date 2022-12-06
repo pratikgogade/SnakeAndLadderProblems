4a1289d8f0f589b4918745a0de609dc42682619c
unknown <Pratik@Example.com>
2022-12-07 00:50:15 +0530

    [Pratik]Add:Player At Position 0

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace SnakeAndLadderProblems
+{
+    public class Programs
+    {
+        static void Main(string[] args)
+        {
+            bool Flag = true;
+            while (Flag)
+            {
+                Console.WriteLine("Welcome to Snake And Ladder Game");
+                Console.WriteLine("1.Start " + "\n" + "2.Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        StartGame startGame = new StartGame();
+                        startGame.Game();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SnakeAndLadderProblems/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblems
+{
+    internal class StartGame
+    {
+        public void Game()
+        {
+            int startPos = 0;
+            Console.WriteLine("Welcome to Snake And Ladder Games Player Is On Position " + startPos);
+        }
+    }
+}
