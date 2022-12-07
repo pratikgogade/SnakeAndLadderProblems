150f6ff4b15439141017656b6a25851a554aa211
unknown <Pratik@Example.com>
2022-12-07 12:12:13 +0530

    [Pratik]Add:Checking For Options

---
+++ SnakeAndLadderProblems/CheckingOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblems
+{
+    internal class CheckOptions
+    {
+        public void Check()
+        {
+            int position = 0;
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("Die roll value " + die);
+            int num = random.Next(1, 4);
+            Console.WriteLine("Player Gets option " + num);
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Player Stays In The Same Position");
+                    break;
+                case 2:
+                    position += die;
+                    Console.WriteLine("Player Moves Ahead And Player Position Is " + position);
+                    break;
+                default:
+                    position -= die;
+                    Console.WriteLine("Player Moves Behind And Player Position Is " + position);
+                    break;
+            }
+        }
+    }
+}
--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -9,7 +9,7 @@ namespace SnakeAndLadderProblems
             while (Flag)
             {
                 Console.WriteLine("Welcome to Snake And Ladder Game");
-                Console.WriteLine("1.Start " + "\n" + "2.DiceRoll" + "\n" + "3.Exit");
+                Console.WriteLine("1.Start " + "\n" + "2.DiceRoll" + "\n" + "3.CheckOptions" + "\n" + "4.Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
@@ -21,7 +21,10 @@ namespace SnakeAndLadderProblems
                         DiceRoll diceRoll = new DiceRoll();
                         diceRoll.Diceroll();
                         break;
-
+                    case 3:
+                        CheckOptions checkOptions = new CheckOptions();
+                        checkOptions.Check();
+                        break;
                     default:
                         break;
                 }
